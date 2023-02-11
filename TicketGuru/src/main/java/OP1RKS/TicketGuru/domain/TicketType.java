f86fed810a5a4387788d2fc00d9b0688905ec39e
Jukka-Pekka Sirviö <sirviojp@gmail.com>
2023-02-11 15:16:38 +0200

    korjattu importit javax.persistence. -> jakarta.persistence.

--- TicketGuru/src/main/java/OP1RKS/TicketGuru/domain/TicketType.java
+++ TicketGuru/src/main/java/OP1RKS/TicketGuru/domain/TicketType.java
@@ -9,12 +9,12 @@ import org.springframework.data.annotation.Id;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name= "ticketType")
