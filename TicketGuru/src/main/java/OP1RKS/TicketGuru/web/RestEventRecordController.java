0be5c56e4097e2863822877fd2366a4d314b516f
Heli-DL <heli.delorme@outlook.com>
2023-02-14 17:35:55 +0200

    Lisätty REST Add

--- TicketGuru/src/main/java/OP1RKS/TicketGuru/web/RestEventRecordController.java
+++ TicketGuru/src/main/java/OP1RKS/TicketGuru/web/RestEventRecordController.java
@@ -2,6 +2,8 @@ package OP1RKS.TicketGuru.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import OP1RKS.TicketGuru.domain.EventRecord;
@@ -19,7 +21,11 @@ public class RestEventRecordController {
 	public Iterable<EventRecord> getEventRecords() {
 		return erepo.findAll();
 	};
-	
+	// REST Add
+	@PostMapping("/events")
+	EventRecord newEventRecord (@RequestBody EventRecord newEventRecord) {
+		return erepo.save(newEventRecord);
+	}
 	// REST Update
 	
 	// REST Find by id
