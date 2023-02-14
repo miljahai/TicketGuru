25558257a79dc7ed15320d58b07691f2e3e90758
Jukka-Pekka Sirviö <sirviojp@gmail.com>
2023-02-14 17:18:44 +0200

    lisätty REST GET all EventRecords

---
+++ TicketGuru/src/main/java/OP1RKS/TicketGuru/web/RestEventRecordController.java
@@ -0,0 +1,30 @@
+package OP1RKS.TicketGuru.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import OP1RKS.TicketGuru.domain.EventRecord;
+import OP1RKS.TicketGuru.domain.EventRecordRepository;
+
+@RestController
+public class RestEventRecordController {
+	
+	@Autowired
+	private EventRecordRepository erepo;
+	
+	// REST EventRecord
+	// REST List all EventRecords
+	@GetMapping("/events/api")
+	public Iterable<EventRecord> getEventRecords() {
+		return erepo.findAll();
+	};
+	
+	// REST Update
+	
+	// REST Find by id
+	
+	// REST Delete
+	
+	
+}
