1704cbac0049b6573bb878748cdeb700c4f30776
Heli-DL <heli.delorme@outlook.com>
2023-03-04 19:43:20 +0200

    Lisätty RestTicketControlleriin SalesEvent assosiaatio

--- TicketGuru/src/main/java/OP1RKS/TicketGuru/web/RestTicketController.java
+++ TicketGuru/src/main/java/OP1RKS/TicketGuru/web/RestTicketController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import OP1RKS.TicketGuru.domain.SalesEventRepository;
 import OP1RKS.TicketGuru.domain.Ticket;
 import OP1RKS.TicketGuru.domain.TicketRepository;
 import OP1RKS.TicketGuru.domain.TicketTypeRepository;
@@ -25,6 +26,10 @@ public class RestTicketController {
 	
 	@Autowired
 	private TicketTypeRepository ttrepo;
+	
+	@Autowired
+	private SalesEventRepository srepo;
+	
 		
 
 	// REST Ticket
@@ -38,9 +43,12 @@ public class RestTicketController {
 	@PostMapping("/tickets")
 	ResponseEntity<Object> newTicket (@RequestBody Ticket newTicket) {
 		Long ticket_type_id = newTicket.getTicketType().getTicket_type_id();
+		Long salesevent_id = newTicket.getSalesEvent().getSalesevent_id();
 		
 		if(!ttrepo.existsById(ticket_type_id)) {
-			return ResponseEntity.badRequest().body("Ticket type with id " + ticket_type_id + " doesn't exist");
+			return ResponseEntity.badRequest().body("TicketType with id " + ticket_type_id + " doesn't exist");
+		} else if (!srepo.existsById(salesevent_id)) {
+			return ResponseEntity.badRequest().body("SalesEvent type with id " + salesevent_id + " doesn't exist");
 		}
 		Ticket savedTicket = trepo.save(newTicket);
 		return ResponseEntity.ok(savedTicket);
@@ -51,11 +59,14 @@ public class RestTicketController {
 	ResponseEntity<Object> editTicket(@RequestBody Ticket editTicket, @PathVariable Long id) {
 		Optional<Ticket> ticket = trepo.findById(id);
 		Long ticket_type_id = editTicket.getTicketType().getTicket_type_id();
+		Long salesevent_id = editTicket.getSalesEvent().getSalesevent_id();
 		
 		if (!ticket.isPresent()) {
 			return ResponseEntity.badRequest().body("Ticket with id " + id + " doesn't exist");
 		} else if (!ttrepo.existsById(ticket_type_id)) {
-			return ResponseEntity.badRequest().body("Ticket type with id " + ticket_type_id + " doesn't exist");
+			return ResponseEntity.badRequest().body("TicketType with id " + ticket_type_id + " doesn't exist");
+		} else if (!srepo.existsById(ticket_type_id)) {
+			return ResponseEntity.badRequest().body("SalesEvent with id " + salesevent_id + " doesn't exist");
 		}
 		
 		Ticket existingTicket = ticket.get();
