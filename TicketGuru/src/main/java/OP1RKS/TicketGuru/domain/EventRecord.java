86f2e065c4c6b5fd99fc4429c5e81daab9950f8c
Jukka-Pekka Sirviö <sirviojp@gmail.com>
2023-03-09 17:21:30 +0200

    Korjattu EventRecordin CascadeType

--- TicketGuru/src/main/java/OP1RKS/TicketGuru/domain/EventRecord.java
+++ TicketGuru/src/main/java/OP1RKS/TicketGuru/domain/EventRecord.java
@@ -5,6 +5,7 @@ import java.util.List;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -49,7 +50,7 @@ public class EventRecord {
 	
 	
 	@JsonIgnore
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "ticket_type_id")
+	@OneToMany(cascade = CascadeType.PERSIST, mappedBy = "ticket_type_id")
 	private List<TicketType> tickettypes;
 	
 	
