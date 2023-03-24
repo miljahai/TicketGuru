be28bf9e5e167f4cce0cc7ece3a46a64881cc74c
Heli-DL <heli.delorme@outlook.com>
2023-03-24 02:13:21 +0200

    Korjattu UserRestControlleria

--- TicketGuru/src/main/java/OP1RKS/TicketGuru/web/UserRestController.java
+++ TicketGuru/src/main/java/OP1RKS/TicketGuru/web/UserRestController.java
@@ -3,17 +3,14 @@ package OP1RKS.TicketGuru.web;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import OP1RKS.TicketGuru.domain.AppUser;
@@ -35,18 +32,6 @@ public class UserRestController {
 	@GetMapping("/users")
 	public Iterable<AppUser> getAppUsers() {
 		return urepo.findAll();
-	};
-	
-	// REST Add
-	@PostMapping("/users")
-	@ResponseStatus(HttpStatus.CREATED)
-	public AppUser newAppUser (@Valid @RequestBody AppUser newAppUser, BindingResult result) throws MethodArgumentNotValidException {
-		if(result.hasErrors()) {
-			throw new MethodArgumentNotValidException(null, result);
-		} 
-		String password = newAppUser.getPassword();
-		newAppUser.setPassword(passwordEncoder.encode(password));
-		return urepo.save(newAppUser);
 	};
 	
 	// REST Update
