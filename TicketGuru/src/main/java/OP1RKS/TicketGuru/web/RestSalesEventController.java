22d6ae9dac066ec1489058dceffe5b8ff99eb6ac
Jukka-Pekka Sirviö <sirviojp@gmail.com>
2023-05-06 10:47:28 +0300

    CORS config

--- TicketGuru/src/main/java/OP1RKS/TicketGuru/web/RestSalesEventController.java
+++ TicketGuru/src/main/java/OP1RKS/TicketGuru/web/RestSalesEventController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import OP1RKS.TicketGuru.domain.SalesEventRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 
+@CrossOrigin(origins = {"http://localhost:8080","https://cen-cenru4.azurewebsites.net", "https://miljahai.github.io"}, maxAge = 3600)
 @RestController
 public class RestSalesEventController {
 	
--- TicketGuru/src/main/java/OP1RKS/TicketGuru/web/RestTicketTypeController.java
+++ TicketGuru/src/main/java/OP1RKS/TicketGuru/web/RestTicketTypeController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import OP1RKS.TicketGuru.domain.TicketTypeRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 
+@CrossOrigin(origins = {"http://localhost:8080","https://cen-cenru4.azurewebsites.net", "https://miljahai.github.io"}, maxAge = 3600)
 @RestController
 public class RestTicketTypeController {
 	
